3f7d383484d2ddec2f16c8b1d12afaea04cf2066
Brian R. Bondy <netzen@gmail.com>
2026-02-22 21:40:20 -0500

    Expand color channel sample to include channel combinations

--- sample_scripts/iflow_color_channels_example.sh
+++ sample_scripts/iflow_color_channels_example.sh
@@ -37,4 +37,37 @@ make_channel "red" "channel-mix path=/0 rr=1 rg=0 rb=0 gr=0 gg=0 gb=0 br=0 bg=0 
 make_channel "green" "channel-mix path=/0 rr=0 rg=0 rb=0 gr=0 gg=1 gb=0 br=0 bg=0 bb=0"
 make_channel "blue" "channel-mix path=/0 rr=0 rg=0 rb=0 gr=0 gg=0 gb=0 br=0 bg=0 bb=1"
 
+combine_channels() {
+  local name="$1"
+  shift
+  local project_path="$OUT_DIR/${INPUT_STEM}_${name}.iflow"
+  local render_path="$CHANNEL_DIR/${INPUT_STEM}_${name}.png"
+  local ops_path="$OUT_DIR/${INPUT_STEM}_${name}.ops"
+
+  "$BIN" new --from-image "$CHANNEL_DIR/${INPUT_STEM}_$1.png" --out "$project_path"
+
+  cat > "$ops_path" <<OPS
+set-layer path=/0 blend=normal opacity=1.0
+OPS
+
+  shift
+  local idx=1
+  for channel in "$@"; do
+    cat >> "$ops_path" <<OPS
+add-layer name=${channel}_pass fill=0,0,0,0 width=1 height=1
+import-image path=/$idx file=$CHANNEL_DIR/${INPUT_STEM}_${channel}.png
+set-layer path=/$idx blend=add opacity=1.0
+OPS
+    idx=$((idx + 1))
+  done
+
+  "$BIN" ops --in "$project_path" --out "$project_path" --ops-file "$ops_path" --render "$render_path"
+  echo "Wrote combined image: $render_path"
+}
+
+combine_channels "red_green" red green
+combine_channels "red_blue" red blue
+combine_channels "green_blue" green blue
+combine_channels "red_green_blue" red green blue
+
 echo "Wrote output folder: $CHANNEL_DIR"
